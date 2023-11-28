53d8a11944fb882b84312bec164af68002c36ebe
Jo Colomban <giorgio.colomban@edu.futuregames.nu>
2023-11-28 09:45:46 +0100

    add array print function

--- Utils/ArrayUtils.h
+++ Utils/ArrayUtils.h
@@ -5,4 +5,13 @@
 #ifndef FUTUREGAMES_ALGOSANDDATASTRUCTURESASSIGNEMENT_ARRAYUTILS_H
 #define FUTUREGAMES_ALGOSANDDATASTRUCTURESASSIGNEMENT_ARRAYUTILS_H
 
+#include <iostream>
+
+void printArray(int array[], int size) {
+    for(int i = 0; i < size; i++) {
+        std::cout << array[i] << '\t';
+    }
+    std::cout << std::endl;
+}
+
 #endif //FUTUREGAMES_ALGOSANDDATASTRUCTURESASSIGNEMENT_ARRAYUTILS_H
